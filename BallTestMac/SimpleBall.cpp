2076e72676c1313668d8451524628e20e0311aab
Kelson Hootman <theK42@gmail.com>
2017-12-02 22:57:39 -0800

    Update mac version of simpleBall to use the new updater style.

--- BallTestMac/SimpleBall.cpp
+++ BallTestMac/SimpleBall.cpp
@@ -73,21 +73,21 @@ void SimpleBall::Init(KEngine2D::MechanicsUpdater * mechanicsSystem, KEngine2D::
 	KEngine2D::StaticTransform initialTransform(position);
 	mMechanics.Init(mechanicsSystem, position, velocity);
 
-	mBoundary.Init(&mMechanics.Get(), radius);
+	mBoundary.Init(&mMechanics, radius);
 	
 	std::vector<KEngine2D::BoundingCircle *> bounds(1, &mBoundary);
 
-	mPhysics.Init(physicsSystem, &mMechanics.Get(), bounds, mass);
+	mPhysics.Init(physicsSystem, &mMechanics, bounds, mass);
 
 	KEngine2D::Point modelUpperLeft = {-radius, -radius};
 	KEngine2D::StaticTransform modelTransform(modelUpperLeft);
 
-	mModelTransform.Init(hierarchySystem, &mMechanics.Get(), modelTransform); 
+	mModelTransform.Init(hierarchySystem, &mMechanics, modelTransform);
 
 	assert(radius <= 4.5); // Current highest supported radius
 	int index = int(abs(radius - .0001) * 2);
 
-	mGraphic.Init(renderer, &circleMaps[index], &mModelTransform.Get());
+	mGraphic.Init(renderer, &circleMaps[index], &mModelTransform);
 	mInitialized = true;
 }
 
